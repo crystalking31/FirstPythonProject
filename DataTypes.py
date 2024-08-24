c6d23a95f36f26a1f24c20982a31bb6f8c982c39
unknown <collinsmunene31@gmail.com>
2024-08-24 09:11:30 +0300

    C:/Users/NENE/PycharmProjects/FirstPythonProject/assingment1.py

--- DataTypes.py
+++ DataTypes.py
@@ -17,12 +17,12 @@ class First:
 
 
 
-#Firstname = "Collins"
-#Middlename = "Munene"
-#Surname = "Gitonga"
+firstname = "Collins"
+Middlename = "Munene"
+Surname = "Gitonga"
 #print(f"{Firstname} {middle name} {surname}")
 #print(Firstname + " " + Middle name+" " + Surname)
 
-First name = input("first name")
+Firstname = input("first name")
 surname = input(enter surname)
-print(f 'YOUR FIRST NAME IS'{first name} ) and your last name is {surname}
+print(f 'YOUR FIRST NAME IS'{firstname} ) and your lastname is {surname}
---
+++ assingment1.py
@@ -0,0 +1,39 @@
+#PERSONAL INFO
+
+Myname = "Collins Munene"
+Iam = "15"
+MyFavouritecolour = "crimson red and sunset yellow "
+ILiveIn = "Nairobi City "
+
+print(Myname)
+print(Iam)
+print(MyFavouritecolour)
+print(ILiveIn)
+
+
+print(f"My name is {Myname}, I am {Iam} years old, I like {MyFavouritecolour}, and I live in {ILiveIn}.")
+
+#numeric variables
+x = 50
+y = 75
+
+print(x+y)
+print(y-x)
+print(x*y)
+print(y/x)
+
+age = 15
+print(age + 5)
+print(f"I five years time I will be {age + 5} ")
+#quote
+FavQuote = "In the darkest times, hope is some thing you have give to yourself. That is true meaning of inner strength "
+
+print(f'{Myname} this my fav quote {FavQuote}')
+#greetings
+FriendName = "Baraka "
+print(f"{Myname} rda  : {FriendName}fiti  ")
+
+
+
+
+
